7a9503cc0cd5b1c44af965a0d999eb11ca100a45
n3mochaken <ns.mixa@gmail.com>
2024-04-18 21:31:01 +0300

    +streams v.3

--- src/main/java/ru/hogwards/school/controllers/StudentController.java
+++ src/main/java/ru/hogwards/school/controllers/StudentController.java
@@ -2,12 +2,15 @@ package ru.hogwards.school.controllers;
 
 
 import org.hibernate.type.IntegerType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwards.school.model.AmountOfStudents;
 import ru.hogwards.school.model.Faculty;
 import ru.hogwards.school.model.Student;
+import ru.hogwards.school.services.AvatarService;
 import ru.hogwards.school.services.StudentService;
 
 import java.util.Collection;
@@ -25,6 +28,8 @@ public class StudentController {
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
+    Logger logger = LoggerFactory.getLogger(StudentController.class);
 
     @GetMapping("/{id}")
     public ResponseEntity getStudentInfo(@PathVariable Long id) {
@@ -108,11 +113,24 @@ public class StudentController {
 
     @GetMapping("/calculateSum")
     public int calculateValue() {
+        long m = System.currentTimeMillis();
+
         int sum = IntStream.rangeClosed(1, 1000000)
                 .parallel()
                 .sum();
+        logger.debug("time " + (double) (System.currentTimeMillis() - m));
         return sum;
     }
+    @GetMapping("/calculateSumTest")
+    public int calculateValueTest() {
+        long m = System.currentTimeMillis();
+        int sum = Stream.iterate(1, a -> a + 1)
+                .limit(1_000_000)
+                .reduce(0, (a, b) -> a + b);
+        logger.debug("time " + (double) (System.currentTimeMillis() - m));
+        return sum;
+    }
+
 
 //    @GetMapping("/ageFilter/{age}")
 //    public ResponseEntity<Collection<Student>> getStudentByAge(@PathVariable int age) {
